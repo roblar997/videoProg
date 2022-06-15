fd5ae8e03ff1a94d20d686253f2e337f4139b571
robertlarsenn <72417170+robertlarsenn@users.noreply.github.com>
2022-06-15 19:45:34 +0200

    ordnet IDE...

--- src/main/java/com/example/demo/servlets/videoServlet.java
+++ src/main/java/com/example/demo/servlets/videoServlet.java
@@ -4,12 +4,16 @@ import javax.servlet.*;
 import javax.servlet.http.*;
 import javax.servlet.annotation.*;
 import java.io.IOException;
+import java.io.PrintWriter;
 
 @WebServlet(name = "videoServlet", value = "/videoServlet")
 public class videoServlet extends HttpServlet {
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-
+        response.setContentType("text/html");
+        PrintWriter out = response.getWriter();
+        out.println("<BODY><p>test123</p></BODY>");
+        out.close();
     }
 
     @Override
