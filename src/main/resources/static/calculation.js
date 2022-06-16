f5b14f388baf0102cc2a362fe2cfbfdbd2f4350a
robertlarsenn <72417170+robertlarsenn@users.noreply.github.com>
2022-06-16 20:23:11 +0200

    ccddf

--- src/main/resources/static/calculation.js
+++ src/main/resources/static/calculation.js
@@ -140,8 +140,8 @@ var timeLineModule = (function(){
             //    this.fenwFeatureTree.update(res.features[key].timeslot,res.features[key].featureNmb,res.features[key].val);
            // }
 
-           for (let key in res.tidslinjer){
-                this.timeLines.push(res.tidslinjer[key])
+            for (const [key, value] of Object.entries(res.tidslinjer)){
+                this.timeLines.push(value)
            }
 
         }).promise();
