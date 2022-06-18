2684adcd9aa3c949bcd6a159cf114d120a668bc4
robertlarsenn <72417170+robertlarsenn@users.noreply.github.com>
2022-06-18 15:16:33 +0200

    awfawaw

--- src/main/java/com/example/demo/wrapper/tidslinjeMethodWrapper.java
+++ src/main/java/com/example/demo/wrapper/tidslinjeMethodWrapper.java
@@ -23,6 +23,27 @@ public class tidslinjeMethodWrapper {
         return timeline;
     }
 
+    @Override
+    public String toString() {
+        return "tidslinjeMethodWrapper{" +
+                "remoteMethod='" + remoteMethod + '\'' +
+                ", timeline='" + timeline + '\'' +
+                '}';
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof tidslinjeMethodWrapper)) return false;
+        tidslinjeMethodWrapper that = (tidslinjeMethodWrapper) o;
+        return Objects.equals(remoteMethod, that.remoteMethod) && Objects.equals(timeline, that.timeline);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(remoteMethod, timeline);
+    }
+
     public void setTimeline(String timeline) {
         this.timeline = timeline;
     }
