bde2cd4f01933a3b02f174fef390c4ceb838496b
robertlarsenn <72417170+robertlarsenn@users.noreply.github.com>
2022-06-19 12:38:04 +0200

    EGEGE

--- src/main/resources/static/calculation.js
+++ src/main/resources/static/calculation.js
@@ -168,7 +168,7 @@ var timeLineModule = (function(){
         }).done((res) => {
 
                for (let key in res){
-                    console.log(res[0].commmand.equals("ADD"))
+                    console.log(res[key])
                    if(String(res[key].commmand).equals("ADD")){
                         console.log(res[key].commmand)
                        this.timeLines.push(res[key].timeline)
