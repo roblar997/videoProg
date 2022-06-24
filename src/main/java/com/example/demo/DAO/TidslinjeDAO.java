5f857963fc73a8b8134b1852f738a5954165eab5
robertlarsenn <72417170+robertlarsenn@users.noreply.github.com>
2022-06-24 10:54:14 +0200

    afaf

--- src/main/java/com/example/demo/DAO/TidslinjeDAO.java
+++ src/main/java/com/example/demo/DAO/TidslinjeDAO.java
@@ -30,19 +30,6 @@ public class TidslinjeDAO {
     private EntityManagerFactory emf;
 
 
-    //public  TidslinjeDAO(){
-     //   this.tidslinjer = new LinkedList<Tidslinje>();
-       // this.emf = Persistence.createEntityManagerFactory("schemaTest");
-        //INIT DATA
-       // Tidslinje tidslinje1 = new Tidslinje(1,"ARE",2655579696709L,2655579696709L,0,2,"RWR",false,true,false);
-       // Tidslinje tidslinje2 = new Tidslinje(2,"rr",2655579696709L,2655579696709L,0,2,"see",false,true,true);
-       // Tidslinje tidslinje3 = new Tidslinje(3,"gse",1555637983792L,2655579696709L,0,2,"ses",false,true,false);
-       // Tidslinje tidslinje4 = new Tidslinje(4,"rr",2655579696709L,2655579696709L,0,2,"RestsWR",false,true,false);
-       // tidslinjer.add(tidslinje1);
-        //tidslinjer.add(tidslinje2);
-        //tidslinjer.add(tidslinje3);
-        //tidslinjer.add(tidslinje4);
-   // }
 
     public List<Tidslinje> getTidslinjer(){
        // EntityManager em = emf.createEntityManager();
@@ -56,23 +43,11 @@ public class TidslinjeDAO {
 
        // EntityManager em = emf.createEntityManager();
         //EntityTransaction tx = em.getTransaction();
-        String sql =  "SELECT * FROM \"schemaTest\".\"Tidslinje\"";
-        List<Tidslinje> tidslinjer = db.query(sql, new BeanPropertyRowMapper(Tidslinje.class));
-        Optional<Tidslinje> tidslinjeOpt = tidslinjer.stream().filter((x)->x.getId() == id).findFirst();
-        if(tidslinjeOpt.isPresent()){
-            Tidslinje preTidslinje = tidslinjeOpt.get();
-            preTidslinje.setDeleted(tidslinje.getDeleted());
-            preTidslinje.setDislike(tidslinje.getDislike());
-            preTidslinje.setEnd(tidslinje.getEnd());
-            preTidslinje.setStart(tidslinje.getStart());
-            preTidslinje.setText(tidslinje.getText());
-            preTidslinje.setUser(tidslinje.getUser());
-            preTidslinje.setTimestampChanged(tidslinje.getTimestampChanged());
-            preTidslinje.setTimestampCreated(tidslinje.getTimestampCreated());
-            preTidslinje.setLike(tidslinje.getLike());
-            return "OK";
-        }
-        return "NOTFOUND";
+        String sql =  "UPDATE \"schemaTest\".\"Tidslinje\" SET user=?, timestampcreated=?, timestampchanged=?, start=?, end=?, text=?, like=?, dislike=?, isdeleted=? WHERE id=?";
+        db.update(sql,tidslinje.getUser(),tidslinje.getTimestampCreated(),tidslinje.getTimestampChanged(),tidslinje.getStart(),tidslinje.getEnd(),tidslinje.getText(),tidslinje.getLike(),tidslinje.getDislike(),tidslinje.getDeleted(),tidslinje.getId());
+
+        return "OK";
+
     }
     public String removeTidsline(Integer id){
 
