4ae6b65e3efa8a7a012b5de35be4928bdfe960cf
robertlarsenn <72417170+robertlarsenn@users.noreply.github.com>
2022-06-24 13:15:40 +0200

    fff

--- src/main/java/com/example/demo/DAO/TidslinjeDAO.java
+++ src/main/java/com/example/demo/DAO/TidslinjeDAO.java
@@ -34,7 +34,7 @@ public class TidslinjeDAO {
     public List<Tidslinje> getTidslinjer(){
        // EntityManager em = emf.createEntityManager();
        // EntityTransaction tx = em.getTransaction();
-        String sql =  "SELECT * FROM \"schemaTest\".\"Tidslinje\" WHERE isdeleted=false";
+        String sql =  "SELECT * FROM \"schemaTest\".\"Tidslinje\" WHERE isdeleted IS False";
         List<Tidslinje> tidslinjer = db.query(sql, new BeanPropertyRowMapper(Tidslinje.class));
         return tidslinjer;
     }
