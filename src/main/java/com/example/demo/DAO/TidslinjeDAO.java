bbad5e94139bf765542fe358879d73c3f780a60c
robertlarsenn <72417170+robertlarsenn@users.noreply.github.com>
2022-06-27 13:07:39 +0200

    sss

--- src/main/java/com/example/demo/DAO/TidslinjeDAO.java
+++ src/main/java/com/example/demo/DAO/TidslinjeDAO.java
@@ -1,7 +1,6 @@
 package com.example.demo.DAO;
 
 import com.example.demo.entities.Tidslinje;
-import com.example.demo.wrapper.tidslinjeCommandWrapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.jdbc.core.BeanPropertyRowMapper;
 import org.springframework.jdbc.core.JdbcTemplate;
@@ -11,13 +10,13 @@ import org.springframework.stereotype.Component;
 import org.springframework.transaction.annotation.Transactional;
 
 import javax.ejb.Stateless;
-import javax.persistence.*;
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.EntityTransaction;
+import javax.persistence.Persistence;
 import java.sql.PreparedStatement;
 import java.sql.Types;
-import java.util.LinkedList;
 import java.util.List;
-import java.util.Optional;
-import java.util.stream.Collectors;
 
 
 @Component
@@ -30,11 +29,29 @@ public class TidslinjeDAO {
 
     private EntityManagerFactory emf;
 
+    public TidslinjeDAO() {
+        //this.emf = Persistence.createEntityManagerFactory("persistence");
+    }
+
 
     @Transactional
     public List<Tidslinje> getTidslinjer(){
-       // EntityManager em = emf.createEntityManager();
-       // EntityTransaction tx = em.getTransaction();
+
+      /*  EntityManager em = emf.createEntityManager();
+        EntityTransaction tx = em.getTransaction();
+
+
+        try {
+            tx.begin();
+            //...
+            tx.commit();
+        } catch (Throwable e) {
+
+            tx.rollback();
+        } finally {
+            em.close();
+        }*/
+
         String sql =  "SELECT * FROM \"schemaTest\".\"Tidslinje\" WHERE \"isdeleted\" IS False";
         List<Tidslinje> tidslinjer = db.query(sql, new BeanPropertyRowMapper(Tidslinje.class));
         return tidslinjer;
@@ -42,8 +59,22 @@ public class TidslinjeDAO {
     @Transactional
     public String changeTidsline(Tidslinje tidslinje, Integer id){
 
-       // EntityManager em = emf.createEntityManager();
-        //EntityTransaction tx = em.getTransaction();
+     /*  EntityManager em = emf.createEntityManager();
+        EntityTransaction tx = em.getTransaction();
+
+
+        try {
+            tx.begin();
+            //...
+            tx.commit();
+        } catch (Throwable e) {
+
+            tx.rollback();
+        } finally {
+            em.close();
+        }*/
+
+
         String sql =  "UPDATE \"schemaTest\".\"Tidslinje\" SET \"user\"=?, \"timestampcreated\"=?, \"timestampchanged\"=?, \"start\"=?, \"end\"=?, \"text\"=?, \"like\"=?, \"dislike\"=?, \"isdeleted\"=? WHERE \"id\"=?";
         db.update(sql,tidslinje.getUser(),tidslinje.getTimestampCreated(),tidslinje.getTimestampChanged(),tidslinje.getStart(),tidslinje.getEnd(),tidslinje.getText(),tidslinje.getLike(),tidslinje.getDislike(),tidslinje.getIsdeleted(),id);
 
@@ -53,16 +84,44 @@ public class TidslinjeDAO {
     @Transactional
     public String removeTidsline(Integer id, Long timestampchanged){
 
+      /*  EntityManager em = emf.createEntityManager();
+        EntityTransaction tx = em.getTransaction();
 
-       String sql  =  "UPDATE \"schemaTest\".\"Tidslinje\" SET \"isdeleted\"=?, \"timestampchanged\"=? WHERE \"id\"=?";
-       db.update(sql,true, timestampchanged,id);
-       return "OK";
+
+        try {
+            tx.begin();
+            //...
+            tx.commit();
+        } catch (Throwable e) {
+
+            tx.rollback();
+        } finally {
+            em.close();
+        }*/
+
+
+        String sql  =  "UPDATE \"schemaTest\".\"Tidslinje\" SET \"isdeleted\"=?, \"timestampchanged\"=? WHERE \"id\"=?";
+        db.update(sql,true, timestampchanged,id);
+        return "OK";
 
     }
 
     @Transactional
     public String reverseDelete(Integer id, Long timestampchanged){
+          /*  EntityManager em = emf.createEntityManager();
+        EntityTransaction tx = em.getTransaction();
 
+
+        try {
+            tx.begin();
+            //...
+            tx.commit();
+        } catch (Throwable e) {
+
+            tx.rollback();
+        } finally {
+            em.close();
+        }*/
 
         String sql  =  "UPDATE \"schemaTest\".\"Tidslinje\" SET \"isdeleted\"=?, \"timestampchanged\"=? WHERE \"id\"=?";
         db.update(sql,false, timestampchanged,id);
@@ -71,7 +130,20 @@ public class TidslinjeDAO {
     }
     @Transactional
     public String eraseDeleted(){
+          /*  EntityManager em = emf.createEntityManager();
+        EntityTransaction tx = em.getTransaction();
 
+
+        try {
+            tx.begin();
+            //...
+            tx.commit();
+        } catch (Throwable e) {
+
+            tx.rollback();
+        } finally {
+            em.close();
+        }*/
 
         String sql  =  "DELETE FROM \"schemaTest\".\"Tidslinje\" WHERE \"isdeleted\"=?";
         db.update(sql,true);
@@ -81,8 +153,22 @@ public class TidslinjeDAO {
     @Transactional
     public Integer addTidslinje(Tidslinje tidslinje){
 
-           // EntityManager em = emf.createEntityManager();
-           // EntityTransaction tx = em.getTransaction();
+           /*  EntityManager em = emf.createEntityManager();
+        EntityTransaction tx = em.getTransaction();
+
+
+        try {
+            tx.begin();
+            //...
+            tx.commit();
+        } catch (Throwable e) {
+
+            tx.rollback();
+        } finally {
+            em.close();
+        }*/
+
+
         KeyHolder keyHolder = new GeneratedKeyHolder();
 
         String sql = "INSERT INTO \"schemaTest\".\"Tidslinje\" (\"user\",\"timestampcreated\",\"timestampchanged\",\"start\",\"end\",\"text\",\"like\",\"dislike\",\"isdeleted\") VALUES(?,?,?,?,?,?,?,?,?)";
@@ -90,8 +176,8 @@ public class TidslinjeDAO {
         db.update(con -> {
             PreparedStatement query = con.prepareStatement(sql, new String[]{"id"});
             query.setString(1, tidslinje.getUser() );
-            query.setObject(2, tidslinje.getTimestampCreated(), java.sql.Types.BIGINT);
-            query. setObject(3, tidslinje.getTimestampChanged(), java.sql.Types.BIGINT);
+            query.setObject(2, tidslinje.getTimestampCreated(), Types.BIGINT);
+            query. setObject(3, tidslinje.getTimestampChanged(), Types.BIGINT);
             query.setInt(4, tidslinje.getStart());
             query.setInt(5,tidslinje.getEnd());
             query.setString(6,tidslinje.getText());
@@ -108,8 +194,21 @@ public class TidslinjeDAO {
     @Transactional
     public List<Tidslinje> getLatestChangedOrAdded(Long timestamp){
 
-        //EntityManager em = emf.createEntityManager();
-        //EntityTransaction tx = em.getTransaction();
+        /*  EntityManager em = emf.createEntityManager();
+        EntityTransaction tx = em.getTransaction();
+
+
+        try {
+            tx.begin();
+            //...
+            tx.commit();
+        } catch (Throwable e) {
+
+            tx.rollback();
+        } finally {
+            em.close();
+        }*/
+
 
         //Get newest changes
         String sql =  "SELECT * FROM \"schemaTest\".\"Tidslinje\" WHERE \"timestampchanged\" >= ? ";
@@ -120,8 +219,22 @@ public class TidslinjeDAO {
     @Transactional
     public List<Tidslinje> getLatestChanged(Long timestamp){
 
-       // EntityManager em = emf.createEntityManager();
-        //EntityTransaction tx = em.getTransaction();
+       /*  EntityManager em = emf.createEntityManager();
+        EntityTransaction tx = em.getTransaction();
+
+
+        try {
+            tx.begin();
+            //...
+            tx.commit();
+        } catch (Throwable e) {
+
+            tx.rollback();
+        } finally {
+            em.close();
+        }*/
+
+
         String sql =  "SELECT * FROM \"schemaTest\".\"Tidslinje\" WHERE \"timestampchanged\" <> \"timestampcreated\" AND \"timestampchanged\" > ? ";
         List<Tidslinje> tidslinjer = db.query(sql,new Long[]{timestamp}, new BeanPropertyRowMapper(Tidslinje.class));
         return tidslinjer;
@@ -129,8 +242,22 @@ public class TidslinjeDAO {
     @Transactional
     public List<Tidslinje> getLatestAdded(Long timestamp){
 
-       // EntityManager em = emf.createEntityManager();
-      //  EntityTransaction tx = em.getTransaction();
+          /*  EntityManager em = emf.createEntityManager();
+        EntityTransaction tx = em.getTransaction();
+
+
+        try {
+            tx.begin();
+            //...
+            tx.commit();
+        } catch (Throwable e) {
+
+            tx.rollback();
+        } finally {
+            em.close();
+        }*/
+
+
         String sql =  "SELECT * FROM \"schemaTest\".\"Tidslinje\" WHERE \"timestampcreated\" > ? ";
         List<Tidslinje> tidslinjer = db.query(sql,new Long[]{timestamp}, new BeanPropertyRowMapper(Tidslinje.class));
         return tidslinjer;
